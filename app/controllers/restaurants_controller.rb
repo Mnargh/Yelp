5d466b39115aa6c8d01fd915aa21f700319207b9
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-16 12:58:00 +0000

    Created restaurant model and migrated to database

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,25 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    # @restaurants = Restaurants.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
