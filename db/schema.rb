e42f16c0d66d979b4b5dc63ef9a7e748428e4b47
Léa R <lea.rosamond@gmail.com>
2018-01-17 14:59:45 +0000

    Creates a form html template for creating and editing restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116151717) do
+ActiveRecord::Schema.define(version: 20180116125445) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
