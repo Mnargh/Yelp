d62a6b75a982ed8a07c796a26695880f689645a3
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-17 10:47:13 +0000

    Added test for add, delete and edit restaurant

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,7 +6,34 @@ feature "Index page" do
       expect(page).to have_content "Restaurants"
     end
 
-    
+    scenario "A user can add a new restaurant" do
+      visit "/restaurants/new"
+      fill_in "restaurant[name]", with: "test name"
+      fill_in "restaurant[description]", with: "test description"
+      click_on "Save Restaurant"
+      expect(page).to have_content "test name"
+    end
 
+    scenario "A user can delete a new restaurant" do
+      visit "/restaurants/new"
+      fill_in "restaurant[name]", with: "test name"
+      fill_in "restaurant[description]", with: "test description"
+      click_on "Save Restaurant"
+      visit '/'
+      click_link('Destroy')
+      expect(page).not_to have_content "test name"
+    end
+
+    scenario "A user can edit a new restaurant" do
+      visit "/restaurants/new"
+      fill_in "restaurant[name]", with: "test name"
+      fill_in "restaurant[description]", with: "test description"
+      click_on "Save Restaurant"
+      visit '/'
+      click_link('Edit')
+      fill_in 'restaurant[name]', with: "test name 2"
+      click_on 'Update Restaurant'
+      expect(page).to have_content "test name 2"
+    end
 
 end
