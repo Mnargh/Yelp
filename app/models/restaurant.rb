d64e537ba9ac9e3bbf1e616abfadef7e5034ed3b
Tom Brandon <tom.j.v.brandon@gmail.com>
2018-01-17 15:38:10 +0000

    created review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
   validates :name, presence: true
   validates :description, presence: true,
                         length: { minimum: 10, maximum: 50 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180116125445) do
+ActiveRecord::Schema.define(version: 20180117153201) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20180116125445) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
 end
