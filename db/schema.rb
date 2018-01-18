f5ce612413f1113256cd28fe23f04cdb3f84c237
Xin Wang <wangxin00163@yahoo.co.uk>
2018-01-18 15:52:34 +0000

    Add signup, signin & logout navigation bar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118122336) do
+ActiveRecord::Schema.define(version: 20180118144726) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180118122336) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
